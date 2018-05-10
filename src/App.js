f8722f1f01ab00ff00ef312ed38112e32321ff95
Krissy <35669945+kmschwabs3@users.noreply.github.com>
2018-05-10 12:49:30 -0500

    checkpoint 6 react to do complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo.js'; //imports the class from the todo.js file and can use the to do in the class as the HTML element
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <ul>
+          <ToDo />
+          <ToDo />
+        </ul>    
       </div>
     );
   }
---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+ import React, { Component } from 'react'; //imports React and React's Component Class
+
+ class ToDo extends Component { //Defomes a Class that Extends Component
+    render() { //Defines a render() method on your class
+     return (
+       <li> A todo will go here </li>
+     );
+   }
+ }
+
+ export default ToDo;  
